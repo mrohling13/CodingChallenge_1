47c2a985646002269a55381e6c01a768714cbc65
mrohling13 <malachi.rohling@yahoo.com>
2025-01-26 21:10:27 -0500

    Task 2: Primitive Data Types

--- cc_1.js
+++ cc_1.js
@@ -1,11 +1,25 @@
 // Task 1: Variable //
-let employeeName = "Malachi";
+let employeeName = "Malcartier";
 const employeeID = 1234;
 var isActive = true;
 
-console.log(employeeName);  
+console.log(employeeName);  // Malcartier
 console.log(typeof employeeName);  
 console.log(employeeID);    
 console.log(typeof employeeID);    
 console.log(isActive);      
 console.log(typeof isActive);     
+
+
+// Task 2: Primitive Data Types //
+let productName = 'HMDD Hoodie'
+const productPrice = 160.00;
+var isAvailable = true;
+
+console.log(productName); // HMDD Hoodie
+console.log(typeof productName); // string
+console.log(productprice); // 160.00
+console.log(typeof productPrice); // number
+console.log(isAvailable); // true
+console.log(typeof isAvailable); // boolean
+
