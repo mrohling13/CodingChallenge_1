de17a636c222e1fcbec3b0da9711719cba731892
mrohling13 <malachi.rohling@yahoo.com>
2025-01-26 20:55:49 -0500

    Task 1: Employee Information

--- cc_1.js
+++ cc_1.js
@@ -1,1 +1,11 @@
+// Task 1: Variable //
+let employeeName = "Malachi";
+const employeeID = 1234;
+var isActive = true;
 
+console.log(employeeName);  
+console.log(typeof employeeName);  
+console.log(employeeID);    
+console.log(typeof employeeID);    
+console.log(isActive);      
+console.log(typeof isActive);     
